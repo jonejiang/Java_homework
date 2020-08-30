b488abdb77e3b83f7b5888b0d1bc82a01bf3d4ee
jonejiang <jonehzainer@163.com>
2020-08-30 23:56:35 +0800

    update files jonejiang 202008302356
    Signed-off-by: jonejiang <jonehzainer@163.com>

--- geektime/spring-boot-starter-jdbc/src/main/java/geektime/springboot/homeworks/springbootstarterjdbc/SpringBootStarterJdbcApplication.java
+++ geektime/spring-boot-starter-jdbc/src/main/java/geektime/springboot/homeworks/springbootstarterjdbc/SpringBootStarterJdbcApplication.java
@@ -1,13 +1,38 @@
 package geektime.springboot.homeworks.springbootstarterjdbc;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
-public class SpringBootStarterJdbcApplication {
-
+@Slf4j
+public class SpringBootStarterJdbcApplication implements CommandLineRunner {
+	@Autowired
+	private DataSource dataSource;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterJdbcApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		showConnection();
+	}
+	
+	private void showConnection() throws SQLException {
+		log.info(dataSource.toString());
+		Connection conn = dataSource.getConnection();
+		log.info(conn.toString());
+		conn.close();
 	}
 
 }
